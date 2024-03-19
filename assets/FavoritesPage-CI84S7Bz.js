6c48e64a69b9926d00d7948c901eb76d977a34da
DarynaSD <dasha.dermanchuk@gmail.com>
2024-03-19 16:06:22 +0200

    Updates

---
+++ assets/FavoritesPage-CI84S7Bz.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-DFORdVWJ.js";const r=()=>e.jsx("section",{children:e.jsx("div",{children:"im FavoritesesPage"})});export{r as default};
--- assets/FavoritesPage-DdpHAsOp.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-D8dunwON.js";const r=()=>e.jsx("section",{children:e.jsx("div",{children:"im FavoritesesPage"})});export{r as default};
