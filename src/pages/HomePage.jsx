2df00acb5a0a4b34eb815b6d6b95fb08d77f836a
DarynaSD <dasha.dermanchuk@gmail.com>
2024-03-23 12:55:05 +0200

    input draft

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,22 @@
-import { Section, Wrap } from "./styled/main.styled";
+import { EyeButton, InputElem, InputWrapper, Section, SvgEye, Wrap } from "./styled/main.styled";
+import svg from "../img/sprite.svg";
 
 export const HomePage = () => {
+
   return (
     <Wrap>
-    <Section>
-      <div>im HomePage</div>
-    </Section>
+      <Section>
+        <div>im HomePage</div>
+
+        <InputWrapper>
+          <InputElem type="text" />
+          <EyeButton>
+            <SvgEye>
+              <use href={`${svg}#icon-eye`}></use>
+            </SvgEye>
+          </EyeButton>
+        </InputWrapper>
+      </Section>
     </Wrap>
   );
 };
-
--- src/pages/styled/main.styled.js
+++ src/pages/styled/main.styled.js
@@ -21,3 +21,43 @@ export const Wrap = styled('div')({
   maxWidth: '1440px',
   margin: '97px auto 0',
 });
+
+export const InputWrapper = styled.div`
+position: relative;
+width: 438px;
+height: 52px;
+`
+
+export const InputElem = styled.input`
+height: 100%;
+width: 100%;
+
+padding: 16px 18px;
+border: 1px solid rgba(17, 16, 28, 0.1);
+border-radius: 12px;
+background-color: rgba(251, 251, 251, 1);
+`
+
+export const EyeButton = styled.button`
+
+position: absolute;
+top: 16px;
+right: 18px;
+
+border: none;
+margin: none;
+background-color: transparent;
+
+display: flex;
+align-items: center;
+justify-content: center;
+`
+
+export const SvgEye = styled.svg`
+width: 18px;
+height: 18px;
+
+display: flex;
+align-items: center;
+justify-content: center;
+`
